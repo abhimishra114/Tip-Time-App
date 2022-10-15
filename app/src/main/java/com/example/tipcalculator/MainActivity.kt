81d315b4f93f057e9bda3090ac20263b46c7a64c
Abhisekh <abhisekhmishra26@gmail.com>
2022-10-15 14:33:05 +0530

    changes made in the UI

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tipcalculator
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tipcalculator.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -16,10 +20,13 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener {
             calculateTip()
         }
+        binding.costOfServiceEditText.setOnKeyListener {
+                view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
     }
 
     fun calculateTip() {
-        val valueInTextField = binding.editText.text.toString()
+        val valueInTextField = binding.costOfServiceEditText.text.toString()
         val cost = valueInTextField.toDoubleOrNull()
         if (cost == null) return
         val selectedId = binding.radioGroup.checkedRadioButtonId
@@ -36,5 +43,15 @@ class MainActivity : AppCompatActivity() {
         NumberFormat.getCurrencyInstance()
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount,formattedTip)
+    }
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
